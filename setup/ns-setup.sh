43bdc2dde9123e265989ab869b9b03a092ce69f7
Sergey Zorchenko <sezorchenko@ozon.ru>
2022-06-13 01:19:34 +0300

    wget->curl and confirmations

--- setup/ns-setup.sh
+++ setup/ns-setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get -y install \
     ca-certificates \
     curl \
     gnupg \
@@ -16,7 +16,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo docker run hello-world
 
@@ -37,7 +37,7 @@ echo
 secret=$(cat /proc/sys/kernel/random/uuid)
 echo "NS_SECRET=$secret" >> .env
 
-wget -q -O docker-compose.yml https://raw.githubusercontent.com/justmara/ns-setup/main/setup/docker-compose.yml
+curl https://raw.githubusercontent.com/justmara/ns-setup/main/setup/docker-compose.yml --output docker-compose.yml
 
 sudo docker compose up -d
 
